d62fd831b7f9b6335688e0b9a1a7fe7af1d05482
hmt <>
2020-11-17 18:20:39 +0100

    more extensive testing

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 from .models import Question
 
 class IndexView(generic.ListView):
@@ -9,11 +10,24 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset (self):
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Excludes any question in the future
+        """
+        return Question.objects.filter(
+                pub_date__lte = timezone.now()
+                ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset (self):
+        """
+        Excludes any question in the future
+        """
+        return Question.objects.filter(
+                pub_date__lte = timezone.now()
+                )
 
 class ResultsView(generic.DetailView):
     model = Question
